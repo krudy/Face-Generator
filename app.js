f9d748e76c807605b82d47315c2966bb059fb8b2
TheLastSpoonbender <krzysztof.rudnicki678@gmail.com>
2024-04-07 11:03:28 +0200

    app design has been changed

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 document.querySelector('#form').addEventListener('submit', getFace);
 
 async function getFace(event) {
-	event.preventDefault();
+    event.preventDefault();
 
-	const gender = document.querySelector('#gender').value;
-	const age = document.querySelector('#age').value;
-	const ethnicity = document.querySelector('#ethnicity').value;
+    const gender = document.querySelector('#gender').value;
+    const age = document.querySelector('#age').value;
+    const ethnicity = document.querySelector('#ethnicity').value;
 
-	const url = `https://face-studio.p.rapidapi.com/generate?gender=${gender}&age=${age}&ethnicity=${ethnicity}`;
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': '80be03a6d4msh3de92304a92c4d4p1a3a32jsn601cbf465564',
-			'X-RapidAPI-Host': 'face-studio.p.rapidapi.com'
-		}
-	};
+    const url = `https://face-studio.p.rapidapi.com/generate?gender=${gender}&age=${age}&ethnicity=${ethnicity}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '80be03a6d4msh3de92304a92c4d4p1a3a32jsn601cbf465564',
+            'X-RapidAPI-Host': 'face-studio.p.rapidapi.com'
+        }
+    };
 
-	try {
-		const response = await fetch(url, options);
-		const result = await response.blob();
-		const image = URL.createObjectURL(result);
-		const imageElement = document.createElement('img');
-		const paragraphElement = document.createElement('p');
-		paragraphElement.textContent = url;
-		imageElement.src = image;
-		document.body.appendChild(paragraphElement);
-		document.body.appendChild(imageElement);
-	} catch (error) {
-		console.error(error);
-	}
+    try {
+        const response = await fetch(url, options);
+        const result = await response.blob();
+        const image = URL.createObjectURL(result);
+
+        // Usuwanie istniejącego elementu img
+        const existingImage = document.querySelector('#image-container img');
+        if (existingImage) {
+            existingImage.parentNode.removeChild(existingImage);
+        }
+
+        // Tworzenie nowego elementu img i dodawanie go do kontenera
+        const imageElement = document.createElement('img');
+        imageElement.src = image;
+        document.querySelector('#image-container').appendChild(imageElement);
+    } catch (error) {
+        console.error(error);
+    }
 }
